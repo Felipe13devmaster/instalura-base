f701a2cba0ba9569e38fb5d18e0048ce54792b5c
Felipe <felipe13rodrigues.fr@gmail.com>
2021-07-27 18:48:46 -0300

    Adicionei um componente.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+function Texto(props) {//componente react
+  return <p>{props.children}</p>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title>My page</Title><Texto>Props aqui</Texto></div>
 }
