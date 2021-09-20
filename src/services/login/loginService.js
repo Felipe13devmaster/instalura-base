26f7cfd2bcc55b61521d9a9138117789b1b9f9ea
Felipe <felipe13rodrigues.fr@gmail.com>
2021-09-20 13:28:43 -0300

    M04 Aula08(Testando Hooks e Services).

--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { destroyCookie, setCookie } from 'nookies';
 
 async function HttpClient(url, { headers, body, ...options }) {
@@ -18,8 +19,8 @@ async function HttpClient(url, { headers, body, ...options }) {
 }
 
 const loginService = {
-  async login({ username, password }) {
-    return HttpClient('https://instalura-api-git-master-omariosouto.vercel.app/api/login', {
+  async login({ username, password }, setCookieModule = setCookie, HttpClientModule = HttpClient) { // Este parametro module é apenas para facilitar a injeção de dependecis nos testes
+    return HttpClientModule('https://instalura-api-git-master-omariosouto.vercel.app/api/login', {
       method: 'POST',
       body: {
         username,
@@ -30,7 +31,11 @@ const loginService = {
         // console.log(respostaConvertida);
         const DAY_IN_SECONDS = 86400;
         const { token } = respostaConvertida.data;
-        setCookie(null, 'APP_TOKEN', token, {
+        const hasToken = token;
+
+        if (!hasToken) throw new Error('Failed to login');
+
+        setCookieModule(null, 'APP_TOKEN', token, {
           path: '/', // '/' indica que qualquer pagina apartir da raiz tera aceeso a este cookie
           maxAge: DAY_IN_SECONDS * 7,
         });// Esse null passamos quando cahamamos essa função do lado do client
@@ -39,8 +44,8 @@ const loginService = {
         };
       });
   },
-  logout() {
-    destroyCookie(null, 'APP_TOKEN');
+  async logout(destroyCookieModule = destroyCookie) { // Este parametro module é apenas para facilitar a injeção de dependecis nos testes
+    destroyCookieModule(null, 'APP_TOKEN');
   },
 };
 
